b45893ec891c60146cc0a5a70898dc1970a14a77
Eduardo <110411487+Esalvador1998@users.noreply.github.com>
2023-06-09 13:24:11 -0400

    Add files via upload

---
+++ EnhanceMail/main.py
@@ -0,0 +1,98 @@
+import streamlit as st
+from langchain import PromptTemplate
+from langchain.llms import OpenAI
+
+template = """
+    Below is an email that may be poorly worded.
+    Your goal is to:
+    - Properly format the email
+    - Convert the input text to a specified tone
+    - Convert the input text to a specified dialect
+
+    Here are some examples different Tones:
+    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
+    - Informal: Went to Barcelona for the weekend. Lots to tell you.  
+
+    Here are some examples of words in different dialects:
+    - English: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants
+    - Spanish: Papitas Fritas, algodon de azucar, apartamento, basura, galleta, dedo verde, estacionamiento, pantalones
+
+    Example Sentences from each dialect:
+    - English: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
+    - Spanish: Me dirigí directamente a la sección de productos agrícolas para tomar algunas verduras frescas, como pimientos y calabacines. Después de eso, me dirigí al departamento de carnes para recoger algunas pechugas de pollo.
+
+    Please start the email with a warm introduction. Add the introduction if you need to.
+    
+    Below is the email, tone, and dialect:
+    TONE: {tone}
+    DIALECT: {dialect}
+    EMAIL: {email}
+    
+    YOUR {dialect} RESPONSE:
+"""
+
+prompt = PromptTemplate(
+    input_variables=["tone", "dialect", "email"],
+    template=template,
+)
+
+def load_LLM(openai_api_key):
+    """Logic for loading the chain you want to use should go here."""
+    # Make sure your openai_api_key is set as an environment variable
+    llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
+    return llm
+
+st.set_page_config(page_title="Eduardo's EnhanceMail", page_icon=":robot:")
+
+col1, col2 = st.columns(2)
+
+
+st.markdown("## Eduardo's EnhanceMail")
+
+def get_api_key():
+    input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input")
+    return input_text
+
+openai_api_key = get_api_key()
+
+col1, col2 = st.columns(2)
+with col1:
+    option_tone = st.selectbox(
+        'Which tone would you like your email to have?',
+        ('Formal', 'Informal'))
+    
+with col2:
+    option_dialect = st.selectbox(
+        'Which Dialect would you like?',
+        ('English', 'Spanish'))
+
+def get_text():
+    input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
+    return input_text
+
+email_input = get_text()
+
+if len(email_input.split(" ")) > 700:
+    st.write("Please enter a shorter email. The maximum length is 700 words.")
+    st.stop()
+
+def update_text_with_example():
+    print ("in updated")
+    st.session_state.email_input = "Dear Team, It has come 2 our attenshun that there has been a blaytent violashun of our ofis pet polisy. We regret to inform yu that sum1 has been secrutly bringing a mischeevus sqwirl named Mr. Nukins to work. Plz refraim frum any furthur sqwirl-relatid shenanigans. Sincirely,The Ofis Paws-idential Commitee"
+
+st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
+
+st.markdown("### Your Enhanced Email:")
+
+if email_input:
+    if not openai_api_key:
+        st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
+        st.stop()
+
+    llm = load_LLM(openai_api_key=openai_api_key)
+
+    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+
+    formatted_email = llm(prompt_with_email)
+
+    st.write(formatted_email)
